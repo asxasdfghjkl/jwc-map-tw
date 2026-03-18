3e0daaa3b143b42c7c6d7761977da1a95b3fbaa6
Jamie Zhang <asxasdfghjkl@gmail.com>
2026-03-18 23:01:38 +0800

    update to 2026 date and title

--- src/VALUES.js
+++ src/VALUES.js
@@ -2,7 +2,7 @@ export const ZOOM_STEP = 10;
 export const ZOOM_MIN = 20;
 export const ZOOM_MAX = 100;
 export const LABELS = {
-    5: '7/4(五)',
-    6: '7/5(六)',
-    7: '7/6(日)',
+    5: '7/3(五)',
+    6: '7/4(六)',
+    7: '7/5(日)',
 }
